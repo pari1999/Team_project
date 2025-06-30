36aca5aa3b27010dea643e75970f0ca997c5209e
Fahad Mollah <fahadmollah43@gmail.com>
2025-06-30 23:04:49 +0200

    Final update

--- sorting_system/braccio/braccio_main.py
+++ sorting_system/braccio/braccio_main.py
@@ -1,13 +1,13 @@
-from braccio import braccio_camera, braccio_object, braccio_serial
-import time
+from color_detection import ColorObjectDetector
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def run():
-    logger.info("[Braccio] Starting Braccio object sorter")
-    frame = braccio_camera.get_frame()
-    if braccio_object.object_detected(frame):
-        braccio_serial.send_command("PICK_OBJECT")
-        time.sleep(2)
+def main():
+    logger.info("Starting Braccio Color Sorting System...")
+    detector = ColorObjectDetector()
+    detector.run() 
+
+if __name__ == "__main__":
+    main()
